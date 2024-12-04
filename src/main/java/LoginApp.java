ee29a5dbf82041cbc2d3df01de76a9495797bebd
Rafay Junaid <rafooaj11@gmail.com>
2024-12-04 23:56:22 +0500

    init

--- src/main/java/LoginApp.java
+++ src/main/java/LoginApp.java
@@ -2,17 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class LoginApp extends JFrame {
     private JTextField emailField;
     private JPasswordField passwordField;
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/softwaretesting";
-    private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "12345678";
+    private static final String DB_URL = "jdbc:mysql://avnadmin:AVNS_BOwgsttXw1DG074BVy8@mysql-1e7edf9b-lhr-b3a4.e.aivencloud.com:25416/softwaretesting?ssl-mode=REQUIRED";
 
     public LoginApp() {
         setTitle("Login Screen");
@@ -23,17 +18,17 @@ public class LoginApp extends JFrame {
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(3, 2, 10, 10));
 
-        // Email Label and Text Field
+// Email Label and Text Field
         panel.add(new JLabel("Email:"));
         emailField = new JTextField();
         panel.add(emailField);
 
-        // Password Label and Password Field
+// Password Label and Password Field
         panel.add(new JLabel("Password:"));
         passwordField = new JPasswordField();
         panel.add(passwordField);
 
-        // Login Button
+// Login Button
         JButton loginButton = new JButton("Login");
         loginButton.addActionListener(new LoginAction());
         panel.add(loginButton);
@@ -58,7 +53,7 @@ public class LoginApp extends JFrame {
 
     private String authenticateUser(String email) {
         String userName = null;
-        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+        try (Connection conn = DriverManager.getConnection(DB_URL)) {
             String query = "SELECT name FROM User WHERE Email = ?";
             PreparedStatement stmt = conn.prepareStatement(query);
             stmt.setString(1, email);
